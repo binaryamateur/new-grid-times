1999db3475652e9c4879973e3c29bcfe34a7e3f4
Souvik Patra <souvikpatra@souvik.patra-C02DT3PXMD6N>
2023-02-09 19:13:53 +0530

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Twitter, Facebook } from 'react-feather';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
@@ -144,6 +145,17 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    justify-content: center;
+    gap: 2rem;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: flex-end;
+  }
+
 `;
 
 const Social = styled.div`
@@ -170,6 +182,17 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    --min-column-width: min(220px, 100%);
+    display: grid;
+    grid-template-columns:
+      repeat(auto-fill, minmax(var(--min-column-width), 1fr));
+    gap: 32px;
+    justify-content: center;
+    padding: 16px;
+  }
+
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +219,10 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media ${QUERIES.laptopAndUp} {
+  align-items: flex-start;
+  } 
 `;
 
 const Logo = styled.a`
