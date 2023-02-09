4d6d01bb6c8522f518971d27874374ee8d233ea5
Souvik Patra <souvikpatra@souvik.patra-C02DT3PXMD6N>
2023-02-09 18:57:41 +0530

    completed secondary content area

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -6,6 +6,7 @@ import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
+import { COLORS, QUERIES } from '../../constants';
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -46,15 +47,57 @@ const SpecialtyStoryGrid = () => {
 
 const Wrapper = styled.div`
   display: grid;
-  gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas: 
+    'markets sports';
+    grid-template-columns: 1fr 1fr;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+@media ${QUERIES.laptopAndUp}{
+  grid-area: markets;
+  border-right: 2px solid ${COLORS.gray[300]};
+  padding-right: 1rem;
+}
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  --min-column-width: min(150px, 100%);
+  display: grid;
+  grid-template-columns:
+    repeat(auto-fill, minmax(var(--min-column-width), 1fr));
+  gap: 16px;
+  padding: 16px;
 
-const SportsSection = styled.section``;
+`;
 
-const SportsStories = styled.div``;
+const SportsSection = styled.section`
+
+
+@media ${QUERIES.tabletAndUp}{
+  overflow: scroll;
+}
+@media ${QUERIES.laptopAndUp}{
+  grid-area: sports;
+  margin-left: 1rem;
+}
+`;
+
+const SportsStories = styled.div`
+
+  --min-column-width: min(150px, 100%);
+  display: grid;
+  grid-template-columns:
+    repeat(auto-fill, minmax(var(--min-column-width), 1fr));
+  gap: 16px;
+  padding: 16px;
+
+  @media ${QUERIES.tabletAndUp}{
+    display: flex;
+  }
+
+`;
 
 export default SpecialtyStoryGrid;
